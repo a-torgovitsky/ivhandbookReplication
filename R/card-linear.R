953cfb90bedcbee7ec7fd8c55f0843bb123a03ef
Alexander Torgovitsky <atorgovitsky@gmail.com>
2024-08-22 09:30:12 -0500

    Make RESET test heteroskedasticity-robust

--- R/card-linear.R
+++ R/card-linear.R
@@ -19,15 +19,30 @@ run_lineariv <- function(df, XList) {
       broom::tidy() %>%
       filter(term == "D") %>%
       select(coef = estimate, se = std.error) -> dfiv
-    make_ramsey(XList[[i]]) %>%
-      stats::lm(data = df, formula = .) %>%
-      lmtest::resettest(type = "fitted") -> rt
-    rt[["parameter"]] <- NULL # Otherwise tidy will issue a warning/complaint/notice
-    rt %>%
-      broom::tidy() %>%
-      select(ramsey = p.value) %>%
-      bind_cols(dfiv, .) %>%
-      mutate(cov = names(XList[i]))
+    # Skip if specification is just a constant
+    # RESET test would never reject here
+    # And yhat2 yhat3 are collinear, which breaks the code below
+    if (length(XList[[i]]) > 1) {
+      rf <- make_ramsey(XList[[i]])
+      rlm <- stats::lm(data = df, formula = rf)
+      df %>%
+        mutate(
+          yhat = rlm$fitted.values,
+          yhat2 = yhat^2,
+          yhat3 = yhat^3
+        ) -> df
+      rf2 <- stats::update.formula(rf, . ~ . + yhat2 + yhat3)
+      rlm2 <- stats::lm(data = df, formula = rf2)
+      lh <- car::linearHypothesis(
+        rlm2, c("yhat2", "yhat3"),
+        white.adjust = "hc1",
+        singular.ok = TRUE
+      )
+      pval <- lh$`Pr(>F)`[2]
+    } else {
+      pval <- 1
+    }
+    dfiv %>% mutate(ramsey = pval, cov = names(XList[i]))
   }) %>%
     bind_rows()
 }
